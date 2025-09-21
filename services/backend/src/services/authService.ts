90aa0ae3f5189959306c1f3ff10c6c10bc9bc5ab
Ana Bulla <ana.bulla@correo.ucu.edu.uy>
2025-09-21 14:48:35 -0300

    vulnerabilidad 7 finalizada

--- services/backend/src/services/authService.ts
+++ services/backend/src/services/authService.ts
@@ -5,9 +5,11 @@ import db from '../db';
 import { User,UserRow } from '../types/user';
 import jwtUtils from '../utils/jwt';
 import ejs from 'ejs';
+import bcrypt from 'bcrypt';
 
 const RESET_TTL = 1000 * 60 * 60;         // 1h
 const INVITE_TTL = 1000 * 60 * 60 * 24 * 7; // 7d
+const SALT_ROUNDS = 10;
 
 class AuthService {
 
@@ -20,10 +22,13 @@ class AuthService {
     // create invite token
     const invite_token = crypto.randomBytes(6).toString('hex');
     const invite_token_expires = new Date(Date.now() + INVITE_TTL);
-    await db<UserRow>('users')
+      const hashedPassword = await bcrypt.hash(user.password, SALT_ROUNDS);
+      await db<UserRow>('users')
       .insert({
         username: user.username,
-        password: user.password,
+        //  Se almacena la contraseña en texto plano. Almacenamiento inseguro.
+        //   Se usara bcrypt para cifrar la contraseña, ya que es ideal para esto.
+        password: hashedPassword,
         email: user.email,
         first_name: user.first_name,
         last_name:  user.last_name,
@@ -64,11 +69,17 @@ class AuthService {
       .where({ id: user.id })
       .first();
     if (!existing) throw new Error('User not found');
+    let updatedPassword = existing.password;
+
+    if (user.password) {
+        updatedPassword = await bcrypt.hash(user.password, SALT_ROUNDS);
+    }
+
     await db<UserRow>('users')
       .where({ id: user.id })
       .update({
         username: user.username,
-        password: user.password,
+        password: updatedPassword,
         email: user.email,
         first_name: user.first_name,
         last_name: user.last_name
@@ -82,8 +93,12 @@ class AuthService {
       .andWhere('activated', true)
       .first();
     if (!user) throw new Error('Invalid email or not activated');
-    if (password != user.password) throw new Error('Invalid password');
-    return user;
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) throw new Error('Invalid credentials');
+
+      return user;
   }
 
   static async sendResetPasswordEmail(email: string) {
@@ -128,10 +143,12 @@ class AuthService {
       .first();
     if (!row) throw new Error('Invalid or expired reset token');
 
-    await db('users')
+    const hashedPassword = await bcrypt.hash(newPassword, SALT_ROUNDS);
+
+      await db('users')
       .where({ id: row.id })
       .update({
-        password: newPassword,
+        password: hashedPassword,
         reset_password_token: null,
         reset_password_expires: null
       });
@@ -144,9 +161,12 @@ class AuthService {
       .first();
     if (!row) throw new Error('Invalid or expired invite token');
 
-    await db('users')
+    const hashedPassword = await bcrypt.hash(newPassword, SALT_ROUNDS);
+
+
+      await db('users')
       .update({
-        password: newPassword,
+        password: hashedPassword,
         invite_token: null,
         invite_token_expires: null
       })
